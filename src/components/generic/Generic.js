720263cd18131b3670138a414477e3ea13e2ddb6
mpv <ariel@gcoop.coop>
2019-11-05 18:24:10 -0300

    Allow custom title in Generic screen

--- src/components/generic/Generic.js
+++ src/components/generic/Generic.js
@@ -3,16 +3,26 @@ import {
   Text,
   View,
 } from 'react-native';
+import PropTypes from 'prop-types';
 import styles from './styles';
 
 class Generic extends React.Component {
   render() {
+    const { title } = this.props;
     return (
       <View style={[styles.container]}>
-        <Text style={[styles.text]}>Generic</Text>
+        <Text style={[styles.text]}>{title}</Text>
       </View>
     );
   }
 }
+
+Generic.propTypes = {
+  title: PropTypes.string,
+};
+
+Generic.defaultProps = {
+  title: 'Generic',
+};
 
 export default Generic;
