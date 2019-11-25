b3396181d653d53f4eab49500046eddb021cf399
mpv <ariel@gcoop.coop>
2019-11-25 16:06:34 -0300

    Don't use FlatList in CheckboxContainer
    REL #41253

--- src/components/Complaint/CheckboxContainer.js
+++ src/components/Complaint/CheckboxContainer.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Checkbox from './Checkbox';
 import {
-  FlatList,
   TouchableHighlight,
+  View,
 } from 'react-native';
+import Checkbox from './Checkbox';
 
 class CheckboxContainer extends React.Component {
-
   render() {
     const { checkboxes } = this.props;
 
     return (
-      <FlatList
-        data={checkboxes}
-        keyExtractor={(item) => item.key}
-        renderItem={({ item }) => (
-          <TouchableHighlight>
+      <View>
+        {
+          checkboxes.map((item) => (
+            <TouchableHighlight key={item.key}>
               <Checkbox name={item.name} value={item.name} />
-          </TouchableHighlight>
-        )}
-      />
+            </TouchableHighlight>
+          ))
+        }
+      </View>
     );
   }
 }
+
+CheckboxContainer.propTypes = {
+  checkboxes: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default CheckboxContainer;
