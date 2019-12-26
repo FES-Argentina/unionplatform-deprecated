1bc7fe95770d40147f47c029ddd3c24071a47255
Daniela Colombo <daniela@gcoop.coop>
2019-12-26 12:38:29 -0300

    REL #41596: pdf's styles

--- src/utils/pdf.js
+++ src/utils/pdf.js
@@ -3,26 +3,111 @@ import RNHTMLtoPDF from 'react-native-html-to-pdf';
 /**
  * Template for building the html string for a complaint.
  */
-export const complaintHtml = (complaint) => `
-  <h1>${complaint.id}</h1>
-  <div style="font-size: 16px">
-    <div class="field"><strong>Nombre:</strong> ${complaint.firstname} ${complaint.lastname}</div>
-    <div class="field"><strong>Empresa:</strong> ${complaint.company}</div>
-    <div class="field"><strong>Antigüedad:</strong> ${complaint.seniority}</div>
-    <div class="field"><strong>Tareas:</strong> ${complaint.tasks}</div>
-    <div class="field"><strong>Problema:</strong> ${complaint.problems}</div>
+export const complaintHtml = (complaint, images) => `
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+td, th {
+  border-bottom: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+  font-size: 12px;
+}
+p{
+  font-size: 10px;
+}
+.header{
+  display: flex;
+}
+.isotipo{
+  width: 70px; text-align: left; font-size: 9px; margin-left: 10px; font-weight: bold;
+}
+.mt{
+  margin-top: 40px;
+}
+.legal{
+  font-size: 6px; color: 'grey';  line-height: 1.6;
+}
+</style>
+  <div class="header">
+    <img height="50" width="50" src="https://tinyurl.com/stye5bu" />
+    <p class="isotipo">Asociación de Personal de Plataformas</p>
   </div>
+  <h4 class="mt">Sistema de Autogestión Electrónico de Denuncias</h4>
+  <p><strong>Denuncia: </strong> ${complaint.problems}</p>
+  <p>${complaint.summary}</p>
+  <table class="mt">
+    <tr>
+      <th>Item</th>
+      <th>Descripcion</th>
+      <th>Pais</th>
+    </tr>
+    <tr>
+      <td>Fecha</td>
+      <td>${complaint.date}</td>
+      <td>Argentina</td>
+    </tr>
+    <tr>
+      <td>Ciudad</td>
+      <td>${complaint.city}</td>
+      <td>Argentina</td>
+    </tr>
+    <tr>
+      <td>Empresa</td>
+      <td>${complaint.company}</td>
+      <td>Argentina</td>
+    </tr>
+    <tr>
+      <td>Id</td>
+      <td>${complaint.id}</td>
+      <td>Argentina</td>
+    </tr>
+  </table>
+  <h4 class="mt">Sobre el usuario</h4>
+  <table>
+    <tr>
+      <th>Item</th>
+      <th>Descripcion</th>
+      <th>Pais</th>
+    </tr>
+    <tr>
+      <td>Nombre y apellido</td>
+      <td>${complaint.firstname} ${complaint.lastname}</td>
+      <td>Argentina</td>
+    </tr>
+    <tr>
+      <td>Teléfono</td>
+      <td>${complaint.phonenumber} </td>
+      <td>Argentina</td>
+    </tr>
+    <tr>
+      <td>Antigüedad</td>
+      <td>${complaint.seniority}</td>
+      <td>Argentina</td>
+    </tr>
+    <tr>
+      <td>Tareas</td>
+      <td>${complaint.tasks}</td>
+      <td>Argentina</td>
+    </tr>
+  </table>
+  <p class="mt legal">Legales:  Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam vitae tempor turpis, sed interdum dolor. Sed sem elit, euismod fringilla dignissim ac, egestas non augue. Vivamus tempus aliquam nibh quis molestie. Sed in neque aliquet, blandit massa quis, pretium dui. Fusce vitae tortor vel nulla vehicula lobortis.
+  Vestibulum id pretium urna. Nullam vulputate massa pharetra commodo dignissim. Vestibulum quis mi enim. </p>
 `;
 
 /**
  * Creates a PDF for complaint.
  */
-export const createPdf = async (complaint) => {
+export const createPdf = async (complaint, images) => {
   const options = {
-    html: complaintHtml(complaint),
+    html: complaintHtml(complaint, images),
     fileName: `denuncia-${complaint.id}`,
   };
 
   const file = await RNHTMLtoPDF.convert(options);
+  //alert(file.filePath);
   return file;
 };
