70b2b5fe4bff157e6756fdc6bd99a050db31af93
Daniela Colombo <daniela@gcoop.coop>
2019-12-27 15:24:43 -0300

    REL #41986: Add splash

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import NavigationService from './src/navigation/NavigationService';
 import Switcher from './src/navigation/Switcher';
+import SplashScreen from 'react-native-splash-screen'
 
 class App extends React.Component {
+  componentDidMount() {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }
+
   render() {
     return (
       <Switcher ref={(navigatorRef) => {
