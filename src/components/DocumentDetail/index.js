1d00a4407bad1cdbba8495abf515e4f663a0221d
Daniela Colombo <daniela@gcoop.coop>
2020-01-10 16:17:12 -0300

    REL #42172: Add download file in DocumentDetail

--- src/components/DocumentDetail/index.js
+++ src/components/DocumentDetail/index.js
@@ -5,39 +5,65 @@ import {
   SafeAreaView,
   ScrollView,
   Image,
+  Share
 } from 'react-native';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { Button } from 'react-native-elements';
-
-import { getDocument } from '../../actions/documents';
 import { Alert } from 'react-native';
-
+import RNFetchBlob from 'rn-fetch-blob'
 import styles from '../styles';
 
 class DocumentDetail extends React.Component {
-  componentDidMount = () => {
-    const { loadDocument } = this.props;
-    loadDocument(this.props.navigation.state.params.id);
-  }
 
-  onShare = () => {
-    Alert.alert('On share');
-  }
+  onShare = async () => {
+    try {
+      const result = await Share.share({
+        message:
+          'Sindicato APP',
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          Alert.alert('On share');
+        } else {
+          Alert.alert('On share else');
+        }
+      } else if (result.action === Share.dismissedAction) {
+        Alert.alert('On share dismissed');
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
+  linkDownload = (uri) => {
+     //console.warn(uri)
+  RNFetchBlob
+    .config({
+      // add this option that makes response data to be stored as a file,
+      // this is much more performant.
+      fileCache : true,
+    })
+    .fetch('GET', uri, {
+      //some headers ..
+    })
+    .then((res) => {
+      // the temp file path
+      console.warn('The file saved to ', res.path())
+    })
+  }
 
   render() {
-    const { data } = this.props;
+    const { item, title, description, uri } = this.props.navigation.state.params.item;
 
     return (
       <ScrollView>
         <View style={styles.containerMargin}>
-          <Text style={styles.titleNews}>{data.title}</Text>
-          <Text style={styles.summaryText}>{data.summary}</Text>
-          <Image
-            source={{ uri: data.photo }}
-            style={styles.photoNews}
-          />
+          <Text style={styles.titleNews}>{title}</Text>
+          <Text style={styles.summaryText}>{description}</Text>
+
+          <Text style={styles.link} onPress={() => this.linkDownload(uri)}>
+            {title}
+          </Text>
         <Button onPress={this.onShare} title="Compartir"/>
         </View>
       </ScrollView>
@@ -46,24 +72,9 @@ class DocumentDetail extends React.Component {
 }
 
 DocumentDetail.propTypes = {
-  data: PropTypes.object,
-  loadDocument: PropTypes.func.isRequired,
   navigation: PropTypes.shape({
     navigate: PropTypes.func.isRequired,
   }).isRequired,
 };
 
-DocumentDetail.defaultProps = {
-  data: {},
-};
-
-const mapStateToProps = (state) => ({
-  data: state.documents.item,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  loadDocument: (id) => dispatch(getDocument(id)),
-});
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(DocumentDetail);
+export default DocumentDetail;
