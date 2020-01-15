3f576feeeb3ce56881af8062a47c1e5284522e4e
mpv <ariel@gcoop.coop>
2020-01-15 15:09:06 -0300

    Make sure only our cookies are used
    We need to handle the cookies because the default behaviour is
    unreliable.

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,20 @@ import axios from 'axios';
 import Config from 'react-native-config';
 import SetCookieParser from 'set-cookie-parser';
 import { store } from '../store';
+import RCTNetworking from 'react-native/Libraries/Network/RCTNetworking';
 
 const api = axios.create({
   withCredentials: false,
-})
+});
+
+/**
+ * Delete cookies.
+ */
+function clearCookies() {
+  return new Promise((success) => {
+    RCTNetworking.clearCookies(success);
+  });
+}
 
 /**
  * Returns an object with headers for api requests.
@@ -18,108 +28,136 @@ function buildHeaders() {
 }
 
 export function getDocumentsRequest() {
-  return api.get(`${Config.API_URL}/documents`)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.log('ERROR', error);
-    });
+  return clearCookies().then(() => {
+    return api.get(`${Config.API_URL}/documents`)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+  });
 }
 
 export function getDocumentRequest(id) {
-  return api.get(`${Config.API_URL}/documents/${id}`)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.log('ERROR', error);
-    });
+  return clearCookies().then(() => {
+    return api.get(`${Config.API_URL}/documents/${id}`)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+  });
 }
 
 export function login(username, password) {
-  return api.post(`${Config.API_URL}/user/login?_format=json`, { name: username, pass: password })
-    .then((response) => {
-      var cookie = SetCookieParser(response.headers['set-cookie'], {decodeValues: true});
-      return {
-        data: response.data,
-        cookie,
-      };
-    });
+  return clearCookies().then(() => {
+    return api.post(`${Config.API_URL}/user/login?_format=json`, { name: username, pass: password })
+      .then((response) => {
+        var cookie = SetCookieParser(response.headers['set-cookie'], {decodeValues: true});
+        return {
+          data: response.data,
+          cookie,
+        };
+      });
+  });
 }
 
 export function loginStatus() {
-  const headers = buildHeaders();
-  return api.get(`${Config.API_URL}/user/login_status?_format=json`, { headers })
-    .then((response) => response.data);
+  return clearCookies().then(() => {
+    const headers = buildHeaders();
+    return api.get(`${Config.API_URL}/user/login_status?_format=json`, { headers })
+      .then((response) => response.data);
+  });
 }
 
 export function updateUser(id, data) {
-  return api.put(`${Config.API_URL}/users/${id}`, { user: data })
-    .then((response) => response.data);
+  return clearCookies().then(() => {
+    return api.put(`${Config.API_URL}/users/${id}`, { user: data })
+      .then((response) => response.data);
+  });
 }
 
 export function getUserRequest(id) {
-  return api.get(`${Config.API_URL}/users/${id}`)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.log('ERROR', error);
-    });
+  return clearCookies().then(() => {
+    return api.get(`${Config.API_URL}/users/${id}`)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+  });
 }
 
 export function getNewsRequest() {
-  return api.get(`${Config.API_URL}/news`)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.log('ERROR', error);
-    });
+  return clearCookies().then(() => {
+    return api.get(`${Config.API_URL}/news`)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+  });
 }
 
 export function getNewRequest(id) {
-  return api.get(`${Config.API_URL}/news/${id}`)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.log('ERROR', error);
-    });
+  return clearCookies().then(() => {
+    return api.get(`${Config.API_URL}/news/${id}`)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+  });
 }
 
 export function setEnrollmentRequest(values) {
-  return api.post(`${Config.API_URL}/enrollments`, { enrollment: values })
-    .then((response) => response.data)
-    .catch((error) => {
-      console.log('ERROR', error);
-    });
+  return clearCookies().then(() => {
+    return api.post(`${Config.API_URL}/enrollments`, { enrollment: values })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+  });
 }
 
 export function setComplaintRequest(values) {
-  return api.post(`${Config.API_URL}/complaints`, { complaint: values })
-    .then((response) => response.data)
-    .catch((error) => {
-      console.log('ERROR', error);
-    });
+  return clearCookies().then(() => {
+    return api.post(`${Config.API_URL}/complaints`, { complaint: values })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+  });
 }
 
 export function changeUserPass(id, data) {
-  return api.put(`${Config.API_URL}/users/${id}`, { pass: data })
-    .then((response) => response.data);
+  return clearCookies().then(() => {
+    return api.put(`${Config.API_URL}/users/${id}`, { pass: data })
+      .then((response) => response.data);
+  });
 }
 
 export function getComplaintsRequest() {
-  return api.get(`${Config.API_URL}/complaints`)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.log('ERROR', error);
-    });
+  return clearCookies().then(() => {
+    return api.get(`${Config.API_URL}/complaints`)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+  });
 }
 
 export function getAlertsRequest() {
-  return api.get(`${Config.API_URL}/alerts`)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.log('ERROR', error);
-    });
+  return clearCookies().then(() => {
+    return api.get(`${Config.API_URL}/alerts`)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+  });
 }
 
 export function setAlertRequest(values) {
-  return api.post(`${Config.API_URL}/alertsform`, { alert: values })
-    .then((response) => response.data)
-    .catch((error) => {
-      console.log('ERROR', error);
-    });
+  return clearCookies().then(() => {
+    return api.post(`${Config.API_URL}/alertsform`, { alert: values })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+  });
 }
