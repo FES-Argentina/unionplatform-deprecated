a78378185da7b9eb350740d31a49de2669d17d2d
mpv <ariel@gcoop.coop>
2020-02-07 17:12:05 -0300

    Remove comments

--- App.js
+++ App.js
@@ -5,8 +5,6 @@ import SplashScreen from 'react-native-splash-screen'
 
 class App extends React.Component {
   componentDidMount() {
-    // do stuff while splash screen is shown
-    // After having done stuff (such as async tasks) hide the splash screen
     SplashScreen.hide();
   }
 
