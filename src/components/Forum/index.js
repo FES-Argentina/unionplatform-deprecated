b921b5fbeac2afba090d2323b4f5c4dffbf287c5
mpv <ariel@gcoop.coop>
2020-04-08 22:17:05 -0300

    Implement back button for the forum pages
    REL #43310

--- src/components/Forum/index.js
+++ src/components/Forum/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BackHandler } from 'react-native';
 import { WebView } from 'react-native-webview';
 import Url from 'url-parse';
 import Config from 'react-native-config';
@@ -6,6 +7,33 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Forum extends React.Component {
+  handleBackButton = () => {
+    // FIXME: We should track the webview canGoBack state but 
+    // onNavigationStateChange is not being called.
+    // https://github.com/react-native-community/react-native-webview/issues/291
+    this.webview.goBack();
+    return true;
+  }
+
+  handleFocus = () => {
+    this.backHandler = BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+  }
+
+  handleBlur = () => {
+    this.backHandler.remove();
+  }
+
+  componentDidMount() {
+    const { navigation } = this.props;
+    this.focusSubscription = navigation.addListener('willFocus', this.handleFocus);
+    this.blurSubscription = navigation.addListener('willBlur', this.handleBlur);
+  }
+
+  componentWillUnmount() {
+    this.focusSubscription.remove();
+    this.blurSubscription.remove();
+  }
+
   render() {
     const uri = new Url(Config.FORUM_URL);
     return (
