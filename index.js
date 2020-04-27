b13095796ddd2d843b435055e67204230e19fc62
mpv <ariel@gcoop.coop>
2020-04-27 19:53:04 -0300

    Handle background messages for notifications

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { AppRegistry, Loading } from 'react-native';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
+import messaging from '@react-native-firebase/messaging';
+import { Notifications } from 'react-native-notifications';
 import App from './App';
 import { name as appName } from './app.json';
 
@@ -16,5 +18,12 @@ const AppWrapper = () => (
     </PersistGate>
   </Provider>
 );
+
+messaging().setBackgroundMessageHandler(async (message) => {
+  Notifications.postLocalNotification({
+    title: message.data.title,
+    body: message.data.body,
+  });
+});
 
 AppRegistry.registerComponent(appName, () => AppWrapper);
