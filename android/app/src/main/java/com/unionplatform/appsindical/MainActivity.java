3666818f7c8968eb78832750b6c6e48ea6f6ae25
mpv <ariel@gcoop.coop>
2020-06-29 14:37:57 -0300

    Rename application

--- android/app/src/main/java/com/unionplatform/appsindical/MainActivity.java
+++ android/app/src/main/java/com/unionplatform/appsindical/MainActivity.java
@@ -1,4 +1,4 @@
-package com.sindicapp;
+package com.unionplatform.appsindical;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
@@ -20,7 +20,7 @@ public class MainActivity extends ReactActivity {
   }   
   @Override
   protected String getMainComponentName() {
-    return "sindicapp";
+    return "appsindical";
   }
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
--- android/app/src/main/java/com/unionplatform/appsindical/MainApplication.java
+++ android/app/src/main/java/com/unionplatform/appsindical/MainApplication.java
@@ -1,4 +1,4 @@
-package com.sindicapp;
+package com.unionplatform.appsindical;
 
 import android.app.Application;
 import android.content.Context;
